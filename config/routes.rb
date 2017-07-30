a7d5c7981609e5e5ba5a25da8990cdb21e5f1c67
quantd2 <dinhquan.tran@gmail.com>
2017-07-31 01:47:20 +0700

    working on admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
   get 'welcome/index'
 
@@ -28,7 +29,9 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    # get 'dashboard/index'
     get '', to: 'dashboard#index', as: '/'
+    resources :polls
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
