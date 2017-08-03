7619e798a296131f62d599e8492987a17555e62d
quantd2 <dinhquan.tran@gmail.com>
2017-08-03 20:41:00 +0700

    config droplet

--- Gemfile
+++ Gemfile
@@ -60,7 +60,7 @@ gem 'actionpack-page_caching'
 gem 'actionpack-action_caching'
 gem 'mail_form'
 gem 'fog'
-gem 'nokogiri'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # Change these
-server '128.199.246.201', port: 22, roles: [:web, :app, :db], primary: true
+server '188.166.248.37', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:quantd2/chondi.git'
 set :application,     'chondi'
-set :user,            'root'
+set :user,            'chondi'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
