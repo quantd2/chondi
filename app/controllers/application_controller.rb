eb354752707640bb2161952d53dbd4ea9eea0e23
quantd2 <dinhquan.tran@gmail.com>
2017-08-04 21:52:27 +0700

    remove csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  
   def admin?
     if !current_user.admin
       flash[:alert] = "Unauthorized access"
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+  skip_before_action :verify_authenticity_token
+
   def update_resource(resource, params)
     if resource.encrypted_password.blank? # || params[:password].blank?
       resource.email = params[:email] if params[:email]
