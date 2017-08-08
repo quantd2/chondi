55f3ff770098baa283127e869860ba12ca8956ba
quantd2 <dinhquan.tran@gmail.com>
2017-08-08 13:14:49 +0700

    add secret

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_KEY'],
-      aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
       region: 'ap-southeast-1'
     }
     config.fog_directory = ENV['S3_BUCKET_NAME']
